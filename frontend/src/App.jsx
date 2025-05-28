cf0c51054744896ddedf432f036f8a232b3f0bd2
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-05-28 16:50:04 -0300

    💻 Commit inicial do projeto Electron com Vite + React + Material UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+function App() {
+
+  return (
+    <>
+      <Typography variant="h6" fontWeight="bold">
+        Teste
+      </Typography>
+    </>
+  )
+}
+
+export default App
