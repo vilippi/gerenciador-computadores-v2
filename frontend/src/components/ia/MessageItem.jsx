afe2a18b14c5ee3798101b08f0a07be5e961eac9
Vitor Lippi <vitorlippilima.vl@gmail.com>
2025-07-23 16:26:17 -0300

    Fix color Mensagens

--- frontend/src/components/ia/MessageItem.jsx
+++ frontend/src/components/ia/MessageItem.jsx
@@ -1,82 +1,42 @@
-
-import {
-    Box,
-    Paper,
-    Typography,
-    Avatar,
-} from '@mui/material';
-import { Person } from '@mui/icons-material';
-import TipsAndUpdatesOutlinedIcon from '@mui/icons-material/TipsAndUpdatesOutlined';
-
 import ReactMarkdown from 'react-markdown';
+import { Box, Typography, Paper, Avatar } from '@mui/material';
+import { Lightbulb, Person } from '@mui/icons-material';
 
 const MessageItem = ({ message }) => {
-    const { text, isAI, images } = message;
+    const isAI = message.isAI;
 
     return (
         <Box
             sx={{
                 display: 'flex',
                 justifyContent: isAI ? 'flex-start' : 'flex-end',
+                alignItems: 'flex-start',
                 mb: 2,
             }}
         >
-            <Box
+            {isAI && (
+                <Avatar sx={{ bgcolor: '#1976d2', mr: 1, width: 32, height: 32 }}>
+                    <Lightbulb fontSize="small" />
+                </Avatar>
+            )}
+
+            <Paper
+                elevation={3}
                 sx={{
-                    display: 'flex',
-                    flexDirection: isAI ? 'row' : 'row-reverse',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    gap: 1,
-                    maxWidth: '75%',
+                    p: 1.5,
+                    maxWidth: '60%',
+                    wordBreak: 'break-word',
+                    borderRadius: 2,
                 }}
             >
-                <Avatar
-                    sx={{
-                        width: 32,
-                        height: 32,
-                        bgcolor: isAI ? 'primary.main' : 'grey.500',
-                    }}
-                >
-                    {isAI ? <TipsAndUpdatesOutlinedIcon fontSize="small" /> : <Person fontSize="small" />}
+                <ReactMarkdown>{message.text}</ReactMarkdown>
+            </Paper>
+
+            {!isAI && (
+                <Avatar sx={{ bgcolor: 'grey.500', ml: 1, width: 32, height: 32 }}>
+                    <Person fontSize="small" />
                 </Avatar>
-                
-                <Paper
-                    sx={{
-                        p: 1.5,
-                        backgroundColor: isAI ? 'grey.100' : 'primary.main',
-                        color: isAI ? 'text.primary' : 'primary.contrastText',
-                        borderRadius: 2,
-                        maxWidth: '100%',
-                        ...(isAI && {
-                            backgroundColor: 'grey.100',
-                            ...(document.documentElement.getAttribute('data-theme') === 'dark' && {
-                                backgroundColor: 'grey.800',
-                            }),
-                        }),
-                    }}
-                >
-                    {images && images.length > 0 && (
-                        <Box sx={{ mb: 1 }}>
-                            {images.map((image, index) => (
-                                <Box
-                                    key={index}
-                                    component="img"
-                                    src={image}
-                                    sx={{
-                                        maxWidth: '200px',
-                                        maxHeight: '200px',
-                                        borderRadius: 1,
-                                        mb: 1,
-                                        display: 'block',
-                                    }}
-                                />
-                            ))}
-                        </Box>
-                    )}
-                    <ReactMarkdown>{message.text}</ReactMarkdown>
-                </Paper>
-            </Box>
+            )}
         </Box>
     );
 };
